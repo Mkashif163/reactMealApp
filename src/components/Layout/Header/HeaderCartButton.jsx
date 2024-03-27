41f70106963f89d374625e1efa1e47f970e38976
Mkashif163 <muhammadkashifbhatti70@gmail.com>
2024-03-27 15:11:28 +0500

    Add CartIcon component and update HeaderCartButton styling

--- src/components/Layout/Header/HeaderCartButton.jsx
+++ src/components/Layout/Header/HeaderCartButton.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import classes from "./HeaderCartButton.module.css";
+import CartIcon from "../../Cart/CartIcon";
 
 const HeaderCartButton = () => {
   return (
-    <button>
-      <span>My Cart</span>
-      <span>3</span>
-      <span>Cart</span>
+    <button className={classes.button}>
+      <span className={classes.icon}>
+        <CartIcon />
+      </span>
+      <span>Your Cart</span>
+      <span className={classes.badge}>3</span>
     </button>
   );
 };
