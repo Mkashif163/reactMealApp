54dd41c26a84799c2310dad39d7570b60beedb5b
Mkashif163 <muhammadkashifbhatti70@gmail.com>
2024-03-28 11:21:39 +0500

    dasda

--- src/components/Meals/MealItem/MealItem.jsx
+++ src/components/Meals/MealItem/MealItem.jsx
@@ -1,7 +1,8 @@
+import { useContext } from "react";
+
 import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
 import CartContext from "../../../store/cart-context";
-import { useContext } from "react";
 
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
--- src/components/Meals/MealItem/MealItemForm.jsx
+++ src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,15 +1,18 @@
-import Input from "../../UI/Input/Input";
-import classes from "./MealItemForm.module.css";
 import { useRef, useState } from "react";
 
+import Input from "../../UI/Input/Input";
+import classes from "./MealItemForm.module.css";
+
 const MealItemForm = (props) => {
-  const amountInputRef = useRef();
   const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
 
   const submitHandler = (event) => {
     event.preventDefault();
+
     const enteredAmount = amountInputRef.current.value;
     const enteredAmountNumber = +enteredAmount;
+
     if (
       enteredAmount.trim().length === 0 ||
       enteredAmountNumber < 1 ||
@@ -18,8 +21,10 @@ const MealItemForm = (props) => {
       setAmountIsValid(false);
       return;
     }
+
     props.onAddToCart(enteredAmountNumber);
   };
+
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <Input
