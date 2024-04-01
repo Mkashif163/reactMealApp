99f8d98dd61ac7ae925555be1fc2d5ad0c4a93bf
Mkashif163 <muhammadkashifbhatti70@gmail.com>
2024-04-01 16:52:00 +0500

    Add Firebase dependency and fetch meals data from Firebase

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,35 +1,37 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
-
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
+import app from "../../firebaseConfig";
+import { getDatabase, ref, get } from "firebase/database";
 
 const AvailableMeals = () => {
+  const [DUMMY_MEALS, setDUMMY_MEALS] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const db = getDatabase(app);
+        const mealsRef = ref(db, "reactMeals/meals");
+        const mealsSnapshot = await get(mealsRef);
+        if (mealsSnapshot.exists()) {
+          const mealsData = mealsSnapshot.val();
+          const loadedMeals = [];
+          for (const mealKey in mealsData) {
+            loadedMeals.push({
+              id: mealKey,
+              name: mealsData[mealKey].name,
+              description: mealsData[mealKey].description,
+              price: mealsData[mealKey].price,
+            });
+          }
+          setDUMMY_MEALS(loadedMeals);
+        }
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    })();
+  }, []);
   return (
     <section className={classes.meals}>
       <Card>
