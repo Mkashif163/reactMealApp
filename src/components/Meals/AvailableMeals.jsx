02e4633ead4c4c2790dab3edc9c7ba71bb9f695b
Mkashif163 <muhammadkashifbhatti70@gmail.com>
2024-04-01 17:59:43 +0500

    Add Checkout component and update Cart component

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -38,17 +38,16 @@ const AvailableMeals = () => {
   return (
     <section className={classes.meals}>
       <Card>
-        <ul>
-          {DUMMY_MEALS.map((meal) => (
-            <MealItem
-              key={meal.id}
-              id={meal.id}
-              name={meal.name}
-              description={meal.description}
-              price={meal.price}
-            />
-          ))}
-        </ul>
+        {isLoading && <p>Loading...</p>}
+        {DUMMY_MEALS.map((meal) => (
+          <MealItem
+            key={meal.id}
+            id={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+          />
+        ))}
       </Card>
     </section>
   );
